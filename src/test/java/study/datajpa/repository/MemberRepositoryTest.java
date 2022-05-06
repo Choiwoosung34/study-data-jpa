20be654aef393ae0d753025d12dcf74031b739dc
Choiwoosung34 <myhouse34@naver.com>
2022-05-06 15:27:53 +0900

    test 코드 추가

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -6,6 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 import study.datajpa.entity.Member;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -25,4 +27,50 @@ class MemberRepositoryTest {
         assertSame(findMember, member);
     }
 
+
+    @Test
+    void basicCRUD() throws Exception {
+        Member member1 = new Member("member1");
+        Member member2 = new Member("member2");
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        // 단건 조회 검증
+        Member findMember1 = memberRepository.findById(member1.getId()).get();
+        Member findMember2 = memberRepository.findById(member2.getId()).get();
+        assertEquals(findMember1, member1);
+        assertEquals(findMember2, member2);
+
+        // 리스트 조회 검증
+        List<Member> all = memberRepository.findAll();
+        assertEquals(2, all.size());
+
+        // 카운트 검증
+        long count = memberRepository.count();
+        assertEquals(2, count);
+
+        // 삭제 검증
+        memberRepository.delete(member1);
+        memberRepository.delete(member2);
+        assertEquals(0, memberRepository.count());
+    }
+
+    @Test
+    void findByUsernameAndAgeGreaterThen() throws Exception {
+        Member m1 = new Member("aaa", 10);
+        Member m2 = new Member("aaa", 20);
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+
+        List<Member> result = memberRepository.findByUsernameAndAgeGreaterThan("aaa", 15);
+
+        assertEquals("aaa", result.get(0).getUsername());
+        assertEquals(20, result.get(0).getAge());
+        assertEquals(1, result.size());
+    }
+
+    @Test
+    public void findHelloBy() {
+        List<Member> helloBy = memberRepository.findTop3HelloBy();
+    }
 }
